dbf1be70516572cf60d274f42f222e5e00d3729d
agathe <agathe@agat.dev>
2025-05-20 14:48:00 +0200

    etats generaux

--- components/map/map-component.tsx
+++ components/map/map-component.tsx
@@ -67,7 +67,7 @@ const defaultCenter = { lat: 46.603354, lng: 2.3522 };
 const defaultZoom = 6;
 
 // Clé API Google Maps
-const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEYS; || process.env.GOOGLE_MAPS_API_KEY;
+const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEYS || process.env.GOOGLE_MAPS_API_KEY;
 const WORDPRESS_API_URL = process.env.WORDPRESS_API_URL || "https://wp-starter.io/wp-json/wp/v2";
 
 export default function MapComponent() {
