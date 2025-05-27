96b6bc88063989f23c0e22961c58f2d2b20b60e9
agathe <agathe@agat.dev>
2025-05-27 10:31:00 +0200

    details

--- components/ui/share-social.tsx
+++ components/ui/share-social.tsx
@@ -21,28 +21,26 @@ const socialPlatforms = [
   {
     name: "Facebook",
     icon: FaFacebookF,
-    url: (u: string, _t?: string, img?: string) =>
-      `https://www.facebook.com/sharer/sharer.php?u=https://lesdoleances.fr${
-        img ? `&picture=https://lesdoleances.fr/img/logo.svg` : ""
-      }`,
+    url: (u: string, t?: string, img?: string) =>
+      `https://www.facebook.com/sharer/sharer.php?u=https://lesdoleances.fr${img ? `&picture=https://lesdoleances.fr/img/logo.svg` : ""}&quote=${t ? t.replace(/&amp;/g, "&").replace(/&quot;/g, '"').replace(/&rsquo;/g, "'") : ""}`,
   },
   {
     name: "LinkedIn",
     icon: FaLinkedinIn,
     url: (u: string, _t?: string) =>
-      `https://www.linkedin.com/sharing/share-offsite/?url=https://lesdoleances.fr`,
+      `https://www.linkedin.com/sharing/share-offsite/?url=https://lesdoleances.fr&title=${_t ? _t.replace(/&amp;/g, "&").replace(/&quot;/g, '"').replace(/&rsquo;/g, "'") + " " : "À découvrir"}`,
   },
   {
     name: "WhatsApp",
     icon: FaWhatsapp,
     url: (u: string, t?: string) =>
-      `https://wa.me/?text=${encodeURIComponent((t ? t + " " : "") + "https://lesdoleances.fr")}`,
+      `https://wa.me/?text=${encodeURIComponent((t ? t.replace(/&amp;/g, "&").replace(/&quot;/g, '"').replace(/&rsquo;/g, "'") + " " : "") + "https://lesdoleances.fr")}`,
   },
   {
     name: "Email",
     icon: FaEnvelope,
     url: (u: string, t?: string) =>
-      `mailto:?subject=${encodeURIComponent(t || "À découvrir")}&body=https://lesdoleances.fr`,
+      `mailto:?subject=${encodeURIComponent(t.replace(/&amp;/g, "&").replace(/&quot;/g, '"').replace(/&rsquo;/g, "'") || "À découvrir")}&body=https://lesdoleances.fr`,
   },
 ]
 
