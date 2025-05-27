5ccbba8fa04a8f3f30da0ba9a42dde07c76d9a2d
agathe <agathe@agat.dev>
2025-05-27 10:53:31 +0200

    details

--- components/ui/share-social.tsx
+++ components/ui/share-social.tsx
@@ -22,7 +22,7 @@ const socialPlatforms = [
     name: "Facebook",
     icon: FaFacebookF,
     url: (u: string, t?: string, img?: string) =>
-      `https://www.facebook.com/sharer/sharer.php?${u}=https://lesdoleances.fr&picture=${img ? img : ""}&quote=${t ? t.replace(/&amp;/g, "&").replace(/&quot;/g, '"').replace(/&rsquo;/g, "'") : ""}`,
+      `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(u)}${img ? `&picture=${encodeURIComponent(img)}` : ""}${t ? `&quote=${encodeURIComponent(t.replace(/&amp;/g, "&").replace(/&quot;/g, '"').replace(/&rsquo;/g, "'"))}` : ""}`,
   },
   {
     name: "LinkedIn",
