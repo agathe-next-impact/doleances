67a039eff91ab0f8406e40c94f2cfaf389956127
agathe <agathe@agat.dev>
2025-06-06 09:34:16 +0200

    sitemap fix v1

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -4,8 +4,21 @@ export async function GET() {
   const res = await fetch('https://wp-starter.io/wp-json/wp/v2/groupe_local?_embed&per_page=100', {
     next: { revalidate: 0 }, // ou cache: 'no-store'
   });
+  const articles = await fetch('https://wp-starter.io/wp-json/wp/v2/posts?_embed&per_page=100', {
+    next: { revalidate: 0 }, // ou cache: 'no-store'
+  });
+  const pages = await fetch('https://wp-starter.io/wp-json/wp/v2/pages?_embed&per_page=100', {
+    next: { revalidate: 0 }, // ou cache: 'no-store'
+  });
+  if (!res.ok || !articles.ok || !pages.ok) {
+    return new Response('Failed to fetch data', { status: 500 });
+  }
 
   const groupes = await res.json();
+  const posts = await articles.json();
+  const postsFiltered = posts.filter((post: any) => post.status === 'publish');
+  const pagesData = await pages.json();
+  const pagesFiltered = pagesData.filter((page: any) => page.status === 'publish');
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8" ?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -14,7 +27,20 @@ export async function GET() {
           return `<url><loc>https://lesdoleances.fr/groupe-local/${g.slug}</loc></url>`;
         })
         .join('\n')}
-    </urlset>`;
+      ${postsFiltered
+        .map((post: any) => {
+          return `<url><loc>https://lesdoleances.fr/article/${post.slug}</loc></url>`;
+        })
+        .join('\n')}
+      ${pagesFiltered
+        .map((page: any) => {
+          return `<url><loc>https://lesdoleances.fr/${page.slug}</loc></url>`;
+        })
+        .join('\n')}
+    </urlset>
+    `;
+
+
 
   return new Response(sitemap, {
     headers: {
