c7791ada2435c774585f04349bc566c8a8f825f3
agathe <agathe@agat.dev>
2025-09-13 16:47:13 +0200

    color wp link

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -29,6 +29,7 @@ export default async function Home() {
   const categories = await fetchCategories()
   categories.sort((a, b) => b.count - a.count)
   const articles = await fetchLastThreePosts()
+  console.log("Articles fetched for homepage:", articles);
   const stickyArticle = articles[0]
 
   return (
